eec2d155786619da1c97fe0395e26d96de59b229
John Mainiero <JCMainiero@gmail.com>
2018-03-20 19:15:56 -0400

    Completed Project

---
+++ resources/sketch.js
@@ -0,0 +1,50 @@
+var snake;
+var pnt = 20;
+var foody;
+var foodx;
+
+function setup(){
+  createCanvas(600,600);
+  snake = new snake();
+  frameRate(10);
+  ranSpawn();
+}
+
+function ranSpawn() {
+  console.log("ranSpawn");
+  var gridC = floor(width/pnt);
+  var gridR = floor(width/pnt);
+  foodx = floor(random(gridC)) * pnt;
+  foody = floor(random(gridR)) * pnt;
+
+  rect(foodx, foody, pnt, pnt);
+}
+
+function draw() {
+  background('rgb(85,107,47)');
+
+  if (snake.eat(foodx,foody)){
+    ranSpawn();
+  }
+  snake.gameOver();
+  snake.update();
+  snake.show();
+
+  fill(255,0,100);
+  rect(foodx, foody, pnt,pnt); //Draw 1st piece of fruit on the map
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.direction(0, -1);
+  }
+  else if (keyCode === DOWN_ARROW){
+    snake.direction(0, 1);
+  }
+  else if (keyCode === LEFT_ARROW){
+    snake.direction(-1, 0);
+  }
+  else if (keyCode === RIGHT_ARROW){
+    snake.direction(1, 0);
+  }
+}
---
+++ resources/snake.js
@@ -0,0 +1,81 @@
+console.log("Snake loaded");
+
+//Creates a snake at x,y and gives the them the following speed.
+function snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 0;
+  this.yspeed = 0;
+  this.total = 0;
+  this.snakeTail = [];
+
+this.direction = function(x,y) {
+  this.xspeed = x;
+  this.yspeed = y;
+}
+
+//Calculate the distance between fruitX,fruitY and this.x this.y
+this.distance = function(numX, numY){
+  var distance;
+
+  var distanceSqrd = (pow((this.x - numX),2)) + (pow((this.y-numY),2));
+  // console.log(distanceSqrd);
+  distance = sqrt(distanceSqrd);
+
+  return distance;
+}
+
+//Compares the fruit(x,y)  to snakePos
+this.eat = function(fruitX, fruitY) {
+
+var distPoints = snake.distance(fruitX,fruitY);
+
+  if(distPoints < 1 ) {
+    // console.log("Less than 1");
+    this.total++;
+    // console.log(this.total);
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+this.gameOver = function() {
+  for(let i=0; i<this.snakeTail.length;i++) {
+    if(snake.distance(this.snakeTail[i].x,this.snakeTail[i].y) < 1) {
+      console.log("Game Over!");
+      alert("GAME OVER!");
+      this.x = 0;
+      this.y = 0;
+      this.snakeTail = [];
+      this.total = 0;
+    }
+  }
+}
+
+this.update = function() {
+  for (var i = 0; i < this.snakeTail.length - 1; i++) {
+        this.snakeTail[i] = this.snakeTail[i + 1];
+      }
+      if (this.total >= 1) {
+        this.snakeTail[this.total - 1] = createVector(this.x, this.y);
+      }
+   this.x = this.x + this.xspeed * pnt;
+   this.y = this.y + this.yspeed * pnt;
+
+   this.x = constrain(this.x, 0, width - pnt);
+   this.y = constrain(this.y, 0, height - pnt);
+
+}
+//Draws the snake on the screen according to the x and y.
+this.show = function() {
+    fill(255);
+    for (var i = 0; i < this.snakeTail.length; i++) {
+      rect(this.snakeTail[i].x, this.snakeTail[i].y, pnt, pnt);
+    }
+    rect(this.x, this.y, pnt, pnt);
+
+  }
+}
